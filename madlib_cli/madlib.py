3bb68dbd6584df632ead73a758de77d54362b893
Hamza-Rashed <62372964+Hamza-Rashed@users.noreply.github.com>
2020-12-02 10:17:39 +0200

    Update madlib.py

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,20 +1,35 @@
 import re
 path = '/home/hamza/Desktop/401/labs/lab3/madlib-cli/textFiles/began.txt'
 def read_template(path):
+    """
+    Create and test a read_template function that takes in a path to text 
+    file and returns a stripped string of the file’s contents.
+    """
     with open(path) as file:
         content = file.read()
     return content
     
 def parse(doc):
+    """
+    Create and test a parse function that takes in a template string and 
+    returns a string with language parts removed and a separate list of those language parts.
+    """
     reg_exp_questions = re.findall(r"\{(.*?)\}", doc)
     return reg_exp_questions
 
 def merge(txt,answers_arr):
+    """
+    Create and test a merge function that takes in a “bare” template and a list of user
+    entered language parts, and returns a string with the language parts inserted into the template.
+    """
     for str_loop in answers_arr:
         txt = txt.replace("{}",str_loop,1)
     return txt
 
 def write():
+    """
+    Create write function for write a new info inside a new file called finall.txt
+    """
     content = read_template(path)
     reg_exp_questions = parse(content)
     answers_arr = []
